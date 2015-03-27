f47d2357c9e905b67228d85697e73cceff545f9f
Kai-Zhang <cs.zhangkai@outlook.com>
2015-03-27 11:23:28 +0800

    Rebuilt the structure of client

--- client/src/main.c
+++ client/src/main.c
@@ -5,22 +5,31 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+int init_sock(char **sys_args) {
+	int sockfd = -1;
+	struct sockaddr_in remote_addr;
+	remote_addr.sin_family = AF_INET;
+	remote_addr.sin_addr.s_addr = inet_addr(sys_args[1]);
+	remote_addr.sin_port = htons(atoi(sys_args[2]));
+	
+	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+		return -1;
+	}
+	if (connect(sockfd, (struct sockaddr *)&remote_addr, sizeof(struct sockaddr)) < 0) {
+		return -1;
+	}
+	return sockfd;
+}
+
+
 int main(int argc, char *argv[]) {
 	if (argc != 3) {
 		puts("Usage: client [server ip] [server port]");
 		return -1;
 	}
+
 	int sockfd = -1;
-	struct sockaddr_in remote_addr;
-	remote_addr.sin_family = AF_INET;
-	remote_addr.sin_addr.s_addr = inet_addr(argv[1]);
-	remote_addr.sin_port = htons(atoi(argv[2]));
-	
-	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("Can not connect to server!\n");
-		return 1;
-	}
-	if (connect(sockfd, (struct sockaddr *)&remote_addr, sizeof(struct sockaddr)) < 0) {
+	if ((sockfd = init_sock(argv)) < 0 ) {
 		perror("Can not connect to server!\n");
 		return 1;
 	}
